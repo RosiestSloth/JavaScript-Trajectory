3591ba273766b751bb0775228757bf574317ca12
RosiestSloth <165097853+RosiestSloth@users.noreply.github.com>
2025-01-05 13:07:26 -0300

    Atualização
    Aqui, adiciono imagens nos outros conteiners e preparo o conteúdo para a segunda seção do site

--- components/cabecalho.vue
+++ components/cabecalho.vue
@@ -1,12 +1,19 @@
 <template>
   <header class="bg-gradient-to-l from-[#033E8C] to-[#1B65A6] text-white p-4 drop-shadow-[0_7px_15px_rgba(0,0,0,0.4)]">
     <h1 class="titulo text-[3em] font-[800] text-shadow-md font-montserrat text-center">Trajetória JavaScript</h1>
-    <nav class=" w-full pt-4 mt-2 border-t border-sky-600">
-      <ul class="flex space-x-4 font-poppins font-semibold">
-        <li><NuxtLink class="hover:text-black hover:bg-blue-400 p-2 rounded-md duration-200" to="/">Home</NuxtLink></li>
-        <li><NuxtLink class="hover:text-black hover:bg-blue-400 p-2 rounded-md duration-200" to="/exercicios">Sobre</NuxtLink></li>
-        <li><NuxtLink class="hover:text-black hover:bg-blue-400 p-2 rounded-md duration-200" to="/experiencia">Minha Experiência</NuxtLink></li>
+    <nav class="w-full pt-4 mt-2 border-t border-sky-600">
+      <ul class="flex flex-wrap gap-4">
+        <li>
+          <NuxtLink class="menu-item text-[1.2em] font-montserrat font-[600] p-2" to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink class="menu-item text-[1.2em] font-montserrat font-[600] p-2" to="/exercicios">Exercícios</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink class="menu-item text-[1.2em] font-montserrat font-[600] p-2" to="/experiencia">Experiência</NuxtLink>
+        </li>
       </ul>
+
     </nav>
   </header>
 </template>
@@ -15,4 +22,29 @@
   .titulo {
     text-shadow: 3px 3px 1px rgba(0, 0, 0, 0.404);
   }
+
+  .menu-item {
+  position: relative;
+}
+
+.menu-item::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: -5px;
+  width: 0;
+  height: 2px;
+  background-color: #000000;
+  transition: width 0.3s ease;
+}
+
+.menu-item:hover::after {
+  width: 100%;
+}
+
+.menu-item:hover {
+  color: #000000;
+  transition-duration: 200ms;
+}
+
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,17 @@
   <cabecalho />
   <main class="p-4 max-w-[1200px] mx-auto bg-white shadow-2xl shadow-white/30">
     <article>
-      <div class="mx-auto mt-4 max-w-7xl py-8 px-4 sm:px-6 md:flex md:items-center md:justify-between">
-        <div class="max-w-[550px] mx-8">
-          <h2 class="bg-white text-black text-[1.4em] font-montserrat font-[600]">
+      <div class="mx-auto mt-4 max-w-7xl py-8 px-4  md:flex md:items-center md:justify-between">
+        <div class="max-w-2xl md:mx-8">
+          <h2 class="bg-white text-black text-[1.4em] font-montserrat font-[600] md:flex-1">
             Aqui, você encontrará todas as páginas desenvolvidas por mim utilizando JavaScript.
           </h2>
           <p class="font-open-sans text-[1.1em] mt-4 text-justify font-[500]">
             Recentemente, concluí um curso de JavaScript com o professor Gustavo Guanabara, onde aprendi os fundamentos dessa linguagem. Motivado a aprimorar minhas habilidades, decidi aplicar o conhecimento adquirido desenvolvendo diversas páginas interativas. Esse processo tem sido fundamental para consolidar meu aprendizado e explorar as possibilidades do JavaScript no desenvolvimento web.
           </p>
         </div>
-        <div class="mt-4 sm:w-[550px]">
-          <h2 class="bg-white text-black text-[1.4em] font-montserrat font-[600]">
+        <div class="mt-4 min-w-[600px] md:w-[550px]">
+          <h2 class="bg-white text-black text-[1.4em] font-montserrat font-[600] md:flex-1">
             Navegue pelas páginas desenvolvidas
           </h2>
           <p class="font-open-sans text-[1.1em] mt-4 text-justify font-[500]">
@@ -26,16 +26,32 @@
           <NuxtLink class="text-white bg-blue-400 p-2 m-4 block w-[200px] text-center border-2 border-black rounded-md duration-200 hover:bg-[#1B65A6] hover:border-white active:bg-[#033E8C]" to="/exercicios">Exercícios Realizados</NuxtLink>
         </div>
       </div>
-      <div class="h-[30vw] min-h-[200px] max-h-[400px] mx-[-16px] bg-[url('/imgs/programador.jpg')] bg-cover bg-fixed bg-center p-8">
-        <div class="backdrop-blur-lg bg-white/20 p-8 rounded-lg shadow-lg ">
+      <div class="h-[30vw] min-h-[360px] max-h-[400px] mx-[-16px] bg-[url('/imgs/programador.jpg')] bg-cover bg-fixed bg-center p-8">
+        <div class="backdrop-blur-lg bg-white/20 p-8 rounded-lg shadow-lg h-full">
           <h2 id="texto-conteiner" class="text-white text-[1.4em] font-montserrat font-[700] text-center">
             Principais exercícios.
           </h2>
           <div id="slide" class="w-full h-full flex justify-center items-center">
-            <div class="border-2 border-white w-[50px] bg-white h-[200px] mx-4 rounded-2xl transition-all duration-500 ease-in-out hover:w-[200px] bg-[url('/imgs/ex-1.jpg')] bg-cover text-white" id="pagina">oiasadsasda</div>
-            <div class="w-[50px] bg-white h-[200px] mx-4 rounded-2xl transition-all duration-500 ease-in-out hover:w-[200px]" id="pagina"></div>
-            <div class="w-[50px] bg-white h-[200px] mx-4 rounded-2xl transition-all duration-500 ease-in-out hover:w-[200px]" id="pagina"></div>
-            <div class="w-[50px] bg-white h-[200px] mx-4 rounded-2xl transition-all duration-500 ease-in-out hover:w-[200px]" id="pagina"></div>
+            <div class="p-4 border-2 border-white w-[50px] h-[200px] mx-4 rounded-2xl transition-all duration-500 ease-in-out hover:w-[200px] bg-[url('/imgs/ex-1.jpg')] bg-cover text-white" id="pagina">
+              <div id="content" class="hidden">
+                conteudo 
+              </div>
+            </div>
+            <div class="p-4 border-2 border-white w-[50px] h-[200px] mx-4 rounded-2xl transition-all duration-500 ease-in-out hover:w-[200px] bg-[url('/imgs/ex-2.jpg')] bg-cover" id="pagina">
+              <div id="content" class="hidden">
+                conteudo 
+              </div>
+            </div>
+            <div class="p-4 border-2 border-white w-[50px] bg-white h-[200px] mx-4 rounded-2xl transition-all duration-500 ease-in-out hover:w-[200px] bg-[url('/imgs/ex-3.jpg')] bg-cover" id="pagina">
+              <div id="content" class="hidden">
+                conteudo 
+              </div>
+            </div>
+            <div class="p-4 border-2 border-white w-[50px] bg-white h-[200px] mx-4 rounded-2xl transition-all duration-500 ease-in-out hover:w-[200px] bg-[url('/imgs/ex-4.jpg')] bg-cover" id="pagina">
+              <div id="content" class="hidden">
+                conteudo 
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -46,16 +62,14 @@
 </template>
 
 <style scoped>
-  h2 {
+  #texto-conteiner h2 {
     text-shadow: 3px 3px 10px rgba(0, 0, 0, 0.479);
   }
 
-  #pagina {
-    display: hidden;
-  }
-
-  #pagina:hover {
+  #pagina:hover #content {
     display: block;
+    overflow: hidden;
+    color: white;
   }
 
 </style>
