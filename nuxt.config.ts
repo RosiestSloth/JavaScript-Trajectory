95d6c7b9867cc1a591f0854ba8d186e1ab6c0353
RosiestSloth <165097853+RosiestSloth@users.noreply.github.com>
2025-01-14 14:36:45 -0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'JavaScript Trajectory',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
   devtools: { enabled: true },
   css: ['./assets/css/style.css'],
   postcss: {
