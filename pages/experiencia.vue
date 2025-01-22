51cb83c07941ffb5eefdfc877b38b919aafcd17f
RosiestSloth <165097853+RosiestSloth@users.noreply.github.com>
2025-01-22 16:52:36 -0300

    Update experiencia.vue

--- pages/experiencia.vue
+++ pages/experiencia.vue
@@ -1,8 +1,21 @@
 <template>
-    <div>
-      <cabecalho />
-      <h1 class="bg-black text-white text-center text-[2em] p-4">
-        Minha Experiência
-      </h1>
-    </div>
+  <div>
+    <cabecalho />
+    <main class="p-4 max-w-[1200px] mx-auto bg-white shadow-2xl shadow-white/30">
+      <article>
+        <h2 class="bg-white text-black text-[1.4em] font-montserrat font-[600]">
+          Minha experiência durante o desenvolvimento
+        </h2>
+        <section>
+          <h3 class="bg-white text-black text-[1.2em] font-montserrat font-[500]">
+            Primeiros 10 exercícios
+          </h3>
+          <p class="font-open-sans text-[1em] mt-4 text-justify font-[400]">
+            Durante o desenvolvimento dos exercícios, aprofundei meus conhecimentos em HTML, CSS e JavaScript. Além disso, refinei minhas habilidades no uso de frameworks. Para este projeto, optei pelo Nuxt.ts, que proporciona uma estrutura robusta e prática, facilitando a criação e organização do projeto.
+          </p>
+        </section>
+      </article>
+    </main>
+    <Rodape />
+  </div>
 </template>
