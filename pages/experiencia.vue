e19161e4119b80638da987fdc97e28e2c698eee1
Vinicius <165097853+RosiestSloth@users.noreply.github.com>
2025-01-23 11:03:31 -0300

    Update experiencia.vue

--- pages/experiencia.vue
+++ pages/experiencia.vue
@@ -10,8 +10,12 @@
           <h3 class="bg-white text-black text-[1.2em] font-montserrat font-[500]">
             Primeiros 10 exercícios
           </h3>
-          <p class="font-open-sans text-[1em] mt-4 text-justify font-[400]">
-            Durante o desenvolvimento dos exercícios, aprofundei meus conhecimentos em HTML, CSS e JavaScript. Além disso, refinei minhas habilidades no uso de frameworks. Para este projeto, optei pelo Nuxt.ts, que proporciona uma estrutura robusta e prática, facilitando a criação e organização do projeto.
+          <p class="font-open-sans text-[1em] mt-4 text-justify font-[400] leading-6 indent-6">
+            Durante o desenvolvimento dos exercícios, aprofundei meus conhecimentos em <strong>HTML</strong>, <strong>CSS</strong> e <strong>JavaScript</strong>. Além disso, refinei minhas habilidades no uso de frameworks. Para este projeto, optei pelo <strong>Nuxt.js</strong> juntamente com <strong>TypeScript</strong>, que proporciona uma estrutura robusta e prática, facilitando a criação e organização do projeto.
+            
+            </p>
+            <p class="font-open-sans text-[1em] mt-4 text-justify font-[400] leading-6 indent-6">
+            Os 10 primeiros exercícios de <strong>JavaScript</strong> foram um marco importante na minha jornada de aprendizado. Encarei alguns desafios com determinação e, ao superá-los, senti uma grande satisfação. A cada exercício concluído, minha confiança e compreensão da linguagem cresciam, tornando a experiência ainda mais enriquecedora.
           </p>
         </section>
       </article>
